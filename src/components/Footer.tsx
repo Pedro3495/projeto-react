34d3bc6fd20a8655b9311d4bd1b297f586c84b48
Pedro Jorge Neves Lira <pedrojorge.dev@gmail.com>
2026-09-22 11:41:49 -0300

    feat: cria estrutura inicial com Tailwind

---
+++ src/components/Footer.tsx
@@ -0,0 +1,6 @@
+
+const Footer = () => {
+  return <p className="text-center">Todos os direitos reservados © CodeLibrary</p>;
+}
+
+export default Footer;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import Header from './components/Header.tsx'
+import Footer from './components/Footer.tsx'
 
-createRoot(document.getElementById('root')!).render(
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <div className="flex flex-col bg-[#F4FBFF] h-screen ">
+      <Header />
+      <div className="flex-1 max-w-[1200px] m-auto w-full">
+        Odair
+      </div>
+      <Footer />
+    </div>
   </StrictMode>,
-)
+);
